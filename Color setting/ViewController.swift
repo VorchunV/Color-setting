51f42cceb8e824652f053a1878644c4f8d2ec0cf
VorchunV <varenkoluchka@gmail.com>
2021-10-22 16:48:27 +0300

    application logic

--- Color setting/ViewController.swift
+++ Color setting/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorField: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorField.layer.cornerRadius = 10
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        redSlider.maximumValue = 1
+        greenSlider.maximumValue = 1
+        blueSlider.maximumValue = 1
+        
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
+        redLabel.font = UIFont(name: "Snell Roundhand Bold" , size: 20)
+        greenLabel.font = UIFont(name: "Snell Roundhand Bold", size: 20)
+        blueLabel.font = UIFont(name: "Snell Roundhand Bold", size: 20)
+        
+        colorField.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+                alpha:1.0
+            )
     }
-
-
-}
+    
+    
+    @IBAction func colorSetting() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        colorField.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+                alpha:1.0
+            )
+    }
+    
+        
+        
+        
+    }
+    
+    
+    
 
